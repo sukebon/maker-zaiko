ebd625bf3aa3db9a1c8e2d86d695eb2d8557fe42
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-12-07 16:03:22 +0900

    my uniportal

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,20 @@
 "use client";
 import React, { FC } from "react";
-import { HeaderList } from "./HeaderList";
 import { HeaderDrawer } from "./HeaderDrawer";
 import { signOut, useSession } from "next-auth/react";
-import { Flex } from "@chakra-ui/react";
-import Link from "next/link";
 
 export const Header: FC = () => {
   const session = useSession();
+  
   const signOutHandler = () => {
     signOut();
   };
+
   return (
     <div className="px-3 flex sticky top-0 bg-blue-900 h-12 z-10 flex items-center">
       <div className="w-full text-white lg:hidden">大丸白衣 WEB在庫</div>
-      <div className="w-full hidden lg:flex gap-3 justify-between mx-3">
-        <div className="flex gap-3 items-center">{/* <HeaderList /> */}</div>
-        <Flex align="center" gap={26}>
+      <div className="w-full hidden lg:flex gap-3 justify-end mx-3">
+        <div>
           {session.data?.user.uid && (
             <div
               className="flex items-center text-white text-sm cursor-pointer"
@@ -25,9 +23,8 @@ export const Header: FC = () => {
               ログアウト
             </div>
           )}
-        </Flex>
+        </div>
       </div>
-
       <HeaderDrawer />
     </div>
   );
